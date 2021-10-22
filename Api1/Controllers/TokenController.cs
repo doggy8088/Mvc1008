7fb59ce905c01b41ff2a8a78f675515f90d10516
Will <doggy.huang@gmail.com>
2021-10-22 17:05:43 +0800

    示範 Token-based Authentication 身份認證與授權

---
+++ Api1/Controllers/TokenController.cs
@@ -0,0 +1,68 @@
+﻿using Api1.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api1.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TokenController : ControllerBase
+    {
+        private readonly JwtHelpers jwt;
+
+        public TokenController(JwtHelpers jwt)
+        {
+            this.jwt = jwt;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("~/signin")]
+        public ActionResult<string> SignIn(LoginViewModel login)
+        {
+            if (ValidateUser(login))
+            {
+                return jwt.GenerateToken(login.Username);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private bool ValidateUser(LoginViewModel login)
+        {
+            return true; // TODO
+        }
+
+        [HttpGet("~/claims")]
+        public IActionResult GetClaims()
+        {
+            return Ok(User.Claims.Select(p => new { p.Type, p.Value }));
+        }
+
+        [HttpGet("~/username")]
+        public IActionResult GetUserName()
+        {
+            return Ok(User.Identity.Name);
+        }
+
+        [HttpGet("~/jwtid")]
+        public IActionResult GetUniqueId()
+        {
+            var jti = User.Claims.FirstOrDefault(p => p.Type == "jti");
+            return Ok(jti.Value);
+        }
+    }
+
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Api1/Startup.cs
+++ Api1/Startup.cs
@@ -1,3 +1,5 @@
+using Api1.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,12 +8,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Mvc1008.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Api1
@@ -29,15 +33,49 @@ namespace Api1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHttpClient();
-
-            services.AddSingleton<ApiClient>(x =>
+            services.AddSingleton(sp =>
             {
                 return new ApiClient(
                     "https://localhost:44335/",
-                    x.GetRequiredService<HttpClient>());
+                    sp.GetRequiredService<HttpClient>());
             });
 
+            services.AddSingleton<JwtHelpers>();
+
             services.AddControllers();
+
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    // 當驗證失敗時，回應標頭會包含 WWW-Authenticate 標頭，這裡會顯示失敗的詳細錯誤原因
+                    options.IncludeErrorDetails = true; // 預設值為 true，有時會特別關閉
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // 透過這項宣告，就可以從 "sub" 取值並設定給 User.Identity.Name
+                        NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+                        // 透過這項宣告，就可以從 "roles" 取值，並可讓 [Authorize] 判斷角色
+                        RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+
+                        // 一般我們都會驗證 Issuer
+                        ValidateIssuer = true,
+                        ValidIssuer = Configuration.GetValue<string>("JwtSettings:Issuer"),
+
+                        // 通常不太需要驗證 Audience
+                        ValidateAudience = false,
+                        //ValidAudience = "JwtAuthDemo", // 不驗證就不需要填寫
+
+                        // 一般我們都會驗證 Token 的有效期間
+                        ValidateLifetime = true,
+
+                        // 如果 Token 中包含 key 才需要驗證，一般都只有簽章而已
+                        ValidateIssuerSigningKey = false,
+
+                        // "1234567890123456" 應該從 IConfiguration 取得
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("JwtSettings:SignKey")))
+                    };
+                });
 
             services.AddSwaggerGen(c =>
             {
@@ -59,6 +97,7 @@ namespace Api1
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
